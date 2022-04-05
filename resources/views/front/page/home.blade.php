b1c0433bb1fe66b675acb155fe1ffd56c1fdfd21
afissabi <afissabi@gmail.com>
2022-04-05 14:50:13 +0700

    new front

--- resources/views/front/page/home.blade.php
+++ resources/views/front/page/home.blade.php
@@ -94,7 +94,7 @@
       <div class="container">
 
         <div class="section-title">
-          <h2>CABANG</h2>
+          <h2>KLINIK KAMI</h2>
           <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
         </div>
 
